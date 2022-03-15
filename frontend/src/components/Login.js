719bfe866e9c6c2e131355c29c51ea3e16809735
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-03-15 11:36:28 +0530

    Built Login page and implemented google auth

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,49 @@
 import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { client } from "../client";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="items-center justify-center m-auto">
+        <div className="text-2xl font-bold mb-5">Pinbay</div>
+        <GoogleLogin
+          clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+          render={(renderProps) => (
+            <button
+              type="button"
+              className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              <FcGoogle className="mr-4" /> Sign in with google
+            </button>
+          )}
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy="single_host_origin"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
